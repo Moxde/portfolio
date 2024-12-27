704ed26eed429393c1c6522e24349c2d940f8b9e
Mox <m.mohamadmurad@yahoo.com>
2024-12-27 22:31:50 +0100

    start1

---
+++ src/Main/Main.tsx
@@ -0,0 +1,13 @@
+
+
+function Main() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>This is the home page of the application.</p>
+        </div>
+    );
+    
+}
+
+export default Main
